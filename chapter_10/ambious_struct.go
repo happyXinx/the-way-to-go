49ed16a269a2943b779777841f7c4673228ec6ed
happyxin <liuxinoe@126.com>
2022-12-25 00:18:51 +0800

    doc(ch10): struct

---
+++ chapter_10/ambious_struct.go
@@ -0,0 +1,23 @@
+package main
+
+//
+//type A struct {
+//	a int
+//}
+//
+//type B struct {
+//	a, b int
+//}
+//
+//type C struct {
+//	A
+//	B
+//}
+
+//
+//func main() {
+//	c := &C{
+//		B: B{a: 1, b: 2},
+//	}
+//	println(c.b)
+//}
